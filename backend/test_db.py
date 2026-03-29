b30fbca2c1c43730d842440018275f99cc318d6f
Manh Nguyen <ncmanh1812@gmail.com>
2026-03-29 09:34:16 +0700

    Test db connection

--- backend/test_db.py
+++ backend/test_db.py
@@ -2,19 +2,16 @@ from app.database.session import SessionLocal
 from app.database.base import User, Product
 
 def test_connection():
-    # Mở một phiên làm việc với Database
     db = SessionLocal()
     try:
-        # Lấy thử người dùng đầu tiên (Admin)
         first_user = db.query(User).first()
-        print(f"👤 User đầu tiên lấy từ DB: {first_user.Username} - {first_user.FullName}")
+        print(f"User đầu tiên lấy từ DB: {first_user.Username} - {first_user.FullName}")
         
-        # Đếm xem có bao nhiêu sản phẩm
         product_count = db.query(Product).count()
-        print(f"📦 Tổng số sản phẩm trong DB: {product_count}")
-        print("✅ KẾT NỐI DATABASE THÀNH CÔNG RỰC RỠ!")
+        print(f"Tổng số sản phẩm trong DB: {product_count}")
+        print("KẾT NỐI DATABASE THÀNH CÔNG")
     except Exception as e:
-        print(f"❌ Có lỗi xảy ra: {e}")
+        print(f"Có lỗi xảy ra: {e}")
     finally:
         db.close()
 
